5a68300f4eb678de8f45f078d088d8c38057eaa4
Daniel <krizdaniel151@gmail.com>
2026-05-13 18:49:07 +0200

    added basic backend and data

--- games/admin.py
+++ games/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Game, Genre, Studio, Platform
+
+admin.site.register(Game)
+admin.site.register(Genre)
+admin.site.register(Studio)
+admin.site.register(Platform)
--- games/models.py
+++ games/models.py
@@ -1,3 +1,56 @@
 from django.db import models
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Platform(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Studio(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+
+    logo = models.ImageField(
+        upload_to='studio_logos/',
+        default='defaults/default_logo.jpg'
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Game(models.Model):
+    title = models.CharField(max_length=100)
+
+    genre = models.ForeignKey(
+        Genre,
+        on_delete=models.CASCADE
+    )
+
+    studio = models.ForeignKey(
+        Studio,
+        on_delete=models.CASCADE
+    )
+
+    platforms = models.ManyToManyField(Platform)
+
+    description = models.TextField(
+        default='No description available.'
+    )
+
+    thumbnail = models.ImageField(
+        upload_to='game_thumbnails/',
+        default='defaults/default_image.jpg'
+    )
+
+    def __str__(self):
+        return self.title
